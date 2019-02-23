42bcbfdd9ec54a2f20e29aa3921e27d8990ea251
Max Hellmich <maxh2709@gmail.com>
2019-02-23 19:32:03 +0100

    Added (functioning, but WIP) detection of Flux Networks config, closes #7

--- src/main/java/lykrast/jetif/compat/CompatFluxNetworks.java
+++ src/main/java/lykrast/jetif/compat/CompatFluxNetworks.java
@@ -1,11 +1,13 @@
 package lykrast.jetif.compat;
 
+import java.io.File;
 import java.util.List;
 
 import lykrast.jetif.JETIFCompat;
 import lykrast.jetif.JETIFWrapper;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
 
@@ -17,8 +19,13 @@ public class CompatFluxNetworks extends JETIFCompat {
 
 	@Override
 	public void addRecipes(List<JETIFWrapper> list) {
-		list.add(new JETIFWrapper(new FluidStack(FluidRegistry.LAVA, 1000), "jetif.fluxnetworks.fire", 
-				getModdedItem("fluxnetworks:flux"), new ItemStack(Items.REDSTONE)));
+		Configuration config = new Configuration(new File("config/flux_networks.cfg"));
+		config.load();
+		boolean fluxEnabled = config.getBoolean("Disables Flux Recipe (from fire)", "flux_recipe", true, "enables Redstone being turned into Flux when dropped in fire");
+		if(fluxEnabled) {
+			list.add(new JETIFWrapper(new FluidStack(FluidRegistry.LAVA, 1000), "jetif.fluxnetworks.fire",
+					getModdedItem("fluxnetworks:flux"), new ItemStack(Items.REDSTONE)));
+		}
 	}
 
 }
