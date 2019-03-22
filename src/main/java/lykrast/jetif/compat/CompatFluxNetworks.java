0b8160619806eb5c9e8f1b7ca6c7d3e3e05e084e
Lykrast <aymericpm@gmail.com>
2019-03-22 22:18:35 +0100

    Detect Flux Networks's config to disable its compat if needed
    Closes #7

--- src/main/java/lykrast/jetif/compat/CompatFluxNetworks.java
+++ src/main/java/lykrast/jetif/compat/CompatFluxNetworks.java
@@ -1,13 +1,16 @@
 package lykrast.jetif.compat;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
+import lykrast.jetif.JETIF;
 import lykrast.jetif.JETIFCompat;
 import lykrast.jetif.JETIFWrapper;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
 public class CompatFluxNetworks extends JETIFCompat {
 
@@ -17,8 +20,23 @@ public class CompatFluxNetworks extends JETIFCompat {
 
 	@Override
 	public void addRecipes(List<JETIFWrapper> list) {
-		list.add(new JETIFWrapper(new FluidStack(FluidRegistry.LAVA, 1000), "jetif.fluxnetworks.fire", 
-				getModdedItem("fluxnetworks:flux"), new ItemStack(Items.REDSTONE)));
+		if (shouldRegister()) {
+			list.add(new JETIFWrapper(new FluidStack(FluidRegistry.LAVA, 1000), "jetif.fluxnetworks.fire", 
+					getModdedItem("fluxnetworks:flux"), new ItemStack(Items.REDSTONE)));
+		}
+	}
+	
+	private boolean shouldRegister() {
+		//Register if config is not found or if it is found and it's active
+		try {
+			Class<?> config = ReflectionHelper.getClass(getClass().getClassLoader(), "sonar.flux.FluxConfig");
+			Field field = ReflectionHelper.findField(config, "enableFluxRecipe");
+			return (Boolean)field.get(null);
+		} catch (Exception e) {
+			JETIF.logger.warn("Couldn't load config field for Flux Networks, report this issue on JETIF's GitHub");
+			e.printStackTrace();
+			return true;
+		}
 	}
 
 }
